cc1e9bb60f3f6afdb690577291b6b0658b5199a5
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-09-12 17:08:33 +0500

    O(1) - Constant Time

--- Big_O/main.js
+++ Big_O/main.js
@@ -13,3 +13,13 @@ function findNemo(array) {
 }
 
 findNemo(large); // O(n) --> Linear Time
+
+
+const boxes = [0,1,2,3,4,5];
+
+function logFirstTwoBoxes(boxes) {
+    console.log(boxes[0]);  // 0(1)
+    console.log(boxes[1]);  // O(1)
+}
+
+logFirstTwoBoxes(boxes);  // O(1) - Constant Time
