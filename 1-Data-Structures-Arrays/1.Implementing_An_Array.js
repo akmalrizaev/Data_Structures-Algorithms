a6ce9499f08745861a540f2d51c3b7b2e7bb2d01
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-09-16 18:39:38 +0500

    Implementing An Array

---
+++ 1-Data-Structures-Arrays/1.Implementing_An_Array.js
@@ -0,0 +1,20 @@
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+
+    get(index) {
+        return this.data[index];
+    }
+
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+}
+
+const newArray = new MyArray();
+newArray.push('hi');
+console.log(newArray);
