6480c90dc15e0576bd34130be8e566431cc361fc
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-09-16 11:01:40 +0500

    Data Structures - Arrays

---
+++ 1-Data-Structures-Arrays/Array.js
@@ -0,0 +1,33 @@
+/*
+
+Array
+
+lookup -> O(1)
+
+push  -> O(1)
+
+insert  -> O(n)
+
+delete  ->  O(n)
+
+*/
+
+const strings = ['a', 'b', 'c', 'd'];
+// 4*4 = 16 bytes of storage in 32bit systems
+
+strings[2] // O(1)
+
+//push
+strings.push('e'); // O(1)
+
+//pop
+strings.pop();  // O(1)
+
+//unshift
+strings.unshift('x');  // O(n)
+
+//splice
+strings.splice(2, 0, 'alien'); // O(n)
+
+
+console.log(strings);
