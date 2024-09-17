534dadd8bee9b2b25dd24fac55bc36eaaf3735f1
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-09-17 08:26:49 +0500

    Implementing An Array

--- 1-Data-Structures-Arrays/1.Implementing_An_Array.js
+++ 1-Data-Structures-Arrays/1.Implementing_An_Array.js
@@ -13,8 +13,34 @@ class MyArray {
         this.length++;
         return this.length;
     }
+
+    pop() {
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
+
+    delete(index) {
+        const item = this.data[index];
+        this.shiftItems(index)
+        return item;
+
+    }
+
+    shiftItems(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length - 1]
+        this.length--
+    }
 }
 
 const newArray = new MyArray();
 newArray.push('hi');
+newArray.push('world');
+// newArray.pop();
+newArray.push('you');
+newArray.delete(1);
 console.log(newArray);
