09877040fa1ea0c62819005e7fa011c3ade34c82
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-09-20 09:59:58 +0500

    First Recuring Character - Naive solution

--- 2-Data-Structures-Hash-Tables/2.First-Recuring-Character.js
+++ 2-Data-Structures-Hash-Tables/2.First-Recuring-Character.js
@@ -7,3 +7,34 @@
 
 //Given an array = [2,3,4,5]:
 //It should return undefined
+
+
+//O(n^2)
+function firstRecurringCharacter(input) {
+    
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+            if (input[i] === input[j]) {
+                
+                return input[i];
+            }
+
+        }
+    }
+    return undefined
+    
+}
+
+function firstRecurringCharacter2(input) {
+    let map = {};
+    for (let i =0; i < input.length; i++) {
+        if(map[input[i]] !== undefined) {
+            return input[i]
+        } else {
+            map[input[i]] = i;
+        }
+    }
+    return undefined;
+}
+
+console.log(firstRecurringCharacter([2,5,5,2,3,4,5]));
