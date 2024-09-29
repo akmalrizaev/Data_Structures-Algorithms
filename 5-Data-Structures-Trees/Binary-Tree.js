a6b26a017b84d1a569e108296c38630950384a54
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-09-29 07:42:20 +0500

    Binary Trees O(log n)

--- 5-Data-Structures-Trees/Binary-Tree.js
+++ 5-Data-Structures-Trees/Binary-Tree.js
@@ -3,3 +3,21 @@ function BinaryTreeNode(value) {
     this.left = null;
     this.right = null;
 }
+
+/*
+
+Level 0: 2^0 = 1;
+Level 1: 2^1 = 2;
+Level 2: 2^2 = 4;
+Level 3: 2^3 = 8;
+
+Number of Nodes = 2^h - 1;
+
+Binary Search Tree
+
+lookup O(log N)
+insert O(log N)
+delete O(log N)
+
+*/
+
