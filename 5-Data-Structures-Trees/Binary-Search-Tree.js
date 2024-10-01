1679583d76171d077ba23e7d41323cf8dcf50c94
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-10-01 15:32:21 +0500

    Binary Search Tree - insert() method

--- 5-Data-Structures-Trees/Binary-Search-Tree.js
+++ 5-Data-Structures-Trees/Binary-Search-Tree.js
@@ -11,6 +11,29 @@ class Node {
       this.root = null;
     }
     insert(value) {
+        const newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            let currentNode = this.root;
+            while(true) {
+                if (value < currentNode.value) {
+                    //Left
+                    if(!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.left;
+                } else {
+                    //Right
+                    if (!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
       
     }
     lookup(value) {
