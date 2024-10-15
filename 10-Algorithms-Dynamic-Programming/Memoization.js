eaa4b73cab401fa14c5a136b33eab478fd7963f1
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-10-15 17:49:14 +0500

    Memoization

--- 10-Algorithms-Dynamic-Programming/Memoization.js
+++ 10-Algorithms-Dynamic-Programming/Memoization.js
@@ -1,3 +1,26 @@
 /*
 Memoization  <> Caching
 */
+
+//learn to cache
+function addTo80(n) {
+    return n + 80;
+  }
+  
+  addTo80(5)
+  
+  let cache = {};
+  function memoizeAddTo80(n) {
+    if (n in cache) {
+      return cache[n];
+    } else {
+      console.log('long time');
+      const answer = n + 80;
+      cache[n] = answer;
+      return answer;
+    }
+  }
+
+  console.log('1', memoizeAddTo80(5));
+  console.log('2',memoizeAddTo80(5));
+  
